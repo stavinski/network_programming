d86d2893665679d0413d67eb5a6cb4036d15c34e
Mike Cromwell <stavinski_2000@yahoo.com>
2017-03-16 08:32:22 +0000

    check permissions

--- scapy/pinger.py
+++ scapy/pinger.py
@@ -5,11 +5,24 @@ import os
 import argparse
 import ipaddress
 import socket
+
+from errno import EACCES, EPERM
 from scapy.all import *
 
 # ICMP types
 ICMP_ECHOREPLY = 0
 ICMP_DEST_UNREACHABLE = 8
+
+def has_previleges():
+    # try and open socket as raw
+    try:
+        socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    except (IOError, OSError) as e:
+        if (e.errno == EACCES or e.errno == EPERM):
+            print "[-] Error: access denied, try running with root previleges"
+            exit(1)
+        else:
+            raise    
 
 def parse_destination(raw):
     dest = None
@@ -30,7 +43,7 @@ def ping(dest, seq):
     start = time.clock()
     id = os.getpid()
     ping = IP(dst=dest)/ICMP(id=id, seq=seq)/payload
-    resp = sr1(ping,verbose=0, timeout=30)
+    resp = sr1(ping,verbose=0, timeout=1)
     duration = (time.clock() - start) * 10
     
     if resp is None:
@@ -52,25 +65,27 @@ def ping(dest, seq):
     
 def main():
     parser = argparse.ArgumentParser('pinger', usage='pinger.py (-d|--destination) [ip or host]')
-    parser.add_argument('-d', '--destination',type=str,help='ip or host address to ping')    
+    parser.add_argument('-d', '--destination',type=str,help='ip or host address to ping')
 
+    has_previleges()
+    
     args = parser.parse_args()
     dest = parse_destination(args.destination)
     
     if dest is None:
-        print "[-] Error: invalid host or ip address entered"
-        exit(1);
+        print ("[-] Error: invalid host or ip address entered");
+        sys.exit(1);
     
-    try:
+    # keep pinging till we get a keyboard interrupt
+    while True:
         seq = 1
         
-        # keep pinging till we get a keyboard interrupt
-        while True:
+        try:    
             ping(dest, seq)
             time.sleep(1)
-            seq += 1
-    except KeyboardInterrupt:
-        pass
-    
+            seq += 1                    
+        except KeyboardInterrupt:
+            break
+        
 if __name__ == "__main__":
     main()
